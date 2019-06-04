c33660de58b41651c9202f31af048c908ef50435
Tien Nguyen <tien.ng1020@gmail.com>
2019-06-04 18:30:49 -0500

    updated

--- javascript/gif.js
+++ javascript/gif.js
@@ -2,33 +2,38 @@ var animals = ["Panda", "Tiger", "Cat", "Dog", "Bird", "Fish", "Chicken"];
 
 function displayAnimal(){
     var animal = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=RpbF7yK9jdJvEIcxqxFvfqB36r3852SI&limit10" ;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=RpbF7yK9jdJvEIcxqxFvfqB36r3852SI&limit=10";
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
+        console.log(response);
+        for (var i=0; i < 10; i++) {
+            var imagesStill = response.data[i].images.downsized_still.url;
+            var imagesAnimate = response.data[i].images.downsized.url;
+            // var animateGiphy = $("<img>").attr("src", animalGif);
+            var image = $("<img>");
+            image.attr("src",imagesStill);
+            image.attr("data-state","still");
+            image.attr("data-animate",imagesAnimate);
+            image.attr("data-still",imagesStill);
+            image.addClass("gif");
+            $("#animalGifs").append(image) ;
+            
+        }
+        
+
         var animalDiv = $("<div class=gifAnimal>");
         var rating = response.data.rating;
         var ratingShow = $("<p>").text("Rating: " + rating);
         animalDiv.append(ratingShow);
-         var animalGif =response.data.url;
-         var animalGiphy = $("<img>").attr("src", animalGif);
-         var dataStill = $("<img>").attr("data-still", response.data.images.original_still.url);
-         $(".gifAnimal").on("click", function() {
-        
-            var state = $(this).attr("data-state");
-           
-            if (state === "still") {
-              $(this).attr("src", $(this).attr("data-animate"));
-              $(this).attr("data-state", "animate");
-            } else {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
-            }
-          });
-       $("#animalGifs").append(animalDiv) ;
-    });
+    })
 }
+        
+           
+           
+       
+
 
 
 function animalButtons () {
@@ -52,15 +57,15 @@ function animalButtons () {
     animalButtons();
 
 
-    $(".gif").on("click", function() {
-        
-        var state = $(this).attr("data-state");
-       
+   
+        $(document).on("click", ".gif", function() {
+                    var state = $(this).attr("data-state");
         if (state === "still") {
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-      });
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+      
