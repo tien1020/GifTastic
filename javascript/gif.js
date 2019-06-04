9eb15e7ab3f7e0665b962b3169edb46194b83a8c
Tien Nguyen <tien.ng1020@gmail.com>
2019-06-04 16:34:18 -0500

    updated

--- javascript/gif.js
+++ javascript/gif.js
@@ -2,13 +2,30 @@ var animals = ["Panda", "Tiger", "Cat", "Dog", "Bird", "Fish", "Chicken"];
 
 function displayAnimal(){
     var animal = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=RpbF7yK9jdJvEIcxqxFvfqB36r3852SI&tag=" + animal;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=RpbF7yK9jdJvEIcxqxFvfqB36r3852SI&limit10" ;
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response){
         var animalDiv = $("<div class=gifAnimal>");
-         animalDiv =response.url;
+        var rating = response.data.rating;
+        var ratingShow = $("<p>").text("Rating: " + rating);
+        animalDiv.append(ratingShow);
+         var animalGif =response.data.url;
+         var animalGiphy = $("<img>").attr("src", animalGif);
+         var dataStill = $("<img>").attr("data-still", response.data.images.original_still.url);
+         $(".gifAnimal").on("click", function() {
+        
+            var state = $(this).attr("data-state");
+           
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
        $("#animalGifs").append(animalDiv) ;
     });
 }
@@ -33,3 +50,17 @@ function animalButtons () {
 
     $(document).on("click", ".animal-btn", displayAnimal);
     animalButtons();
+
+
+    $(".gif").on("click", function() {
+        
+        var state = $(this).attr("data-state");
+       
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
